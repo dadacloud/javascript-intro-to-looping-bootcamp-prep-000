6be6fd516af107618fd8829aa622ff0fc5354890
Marc Jean-Gilles <marcjeangilles@gmail.com>
2018-04-08 18:55:50 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -4,9 +4,9 @@
 
 function forLoop(array){
   for(var i = 0; i < 25; i++){
-    console.log(`I am ${i} strange loops.`);
+    return console.log(`I am ${i} strange loops.`);
   }
-  return array;
+  //return array;
 }
 
 function whileLoop(number){
