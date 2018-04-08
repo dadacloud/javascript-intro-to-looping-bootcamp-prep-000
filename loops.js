be1d431a3befbb21964230022e9711ed1fb448ba
Marc Jean-Gilles <marcjeangilles@gmail.com>
2018-04-08 18:47:00 +0000

    automatically backed up by learn

---
+++ loops.js
@@ -0,0 +1,29 @@
+//for (var i = 1; i < 100; i ++){
+  
+
+
+function forLoop(array){
+  for(let i = 0; i < 25; i++){
+    console.log(`I am ${1} strange loops.`);
+  }
+  return array;
+}
+
+function whileLoop(number){
+  for(let number = number; number--;){
+    console.log(number);
+  }
+  if (number === 0){
+    return `done`;
+  }
+}
+
+function doWhileLoop(array){
+  function maybeTrue() === false{
+    return 
+    
+  }
+  array.length > 0 && maybeTrue()
+  
+  return array;
+}
