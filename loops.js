b7c7a47a0d2b3ee66ce4131ca1e08ea02800d661
Marc Jean-Gilles <marcjeangilles@gmail.com>
2018-04-08 19:45:07 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -25,11 +25,11 @@ function whileLoop(n){
 
 function doWhileLoop(array){
   function maybeTrue() {
-  return Math.random() >= 0.5
+  return Math.random() >= 0.5;
 }
 do{
   array.pop();
-} while 
-}
+} while (array.length > 0 && maybeTrue());
+
   return array;
 }
